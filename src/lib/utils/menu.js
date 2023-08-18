4d00851f5241d00f38279274f63ac89fb117b334
Diego Avendaño <digago7@gmail.com>
2023-08-17 18:15:04 -0600

    first commit

---
+++ src/lib/utils/menu.js
@@ -0,0 +1,11 @@
+import arrachera from '$lib/images/'
+
+export const menu = [
+{
+    nombre: 'hamb',
+    imagen: 'hawaiana',
+    precio: '$35',
+    ingredientes: '50% hamb',
+    detalles: 'juicy'
+}
+]
