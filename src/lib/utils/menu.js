a468c56258762c9f7f637a1d19b4b5b26c87c58b
Diego Avendaño <digago7@gmail.com>
2023-08-19 08:30:20 -0600

    menu

--- src/lib/utils/menu.js
+++ src/lib/utils/menu.js
@@ -1,11 +1,69 @@
-import arrachera from '$lib/images/'
+import arrachera from '$lib/images/menu/arrachera.jpg';
+import champi from '$lib/images/menu/champi.jpg';
+import dedos from '$lib/images/menu/dedos.png';
+import gajo from '$lib/images/menu/gajo.png';
+import hawai from '$lib/images/menu/hawai.png';
+import hotRes from '$lib/images/menu/hot-res.png';
+import french from '$lib/images/menu/french.png';
+import original from '$lib/images/menu/original.png';
+import paloma from '$lib/images/menu/paloma.png';
+import tocinoBbq from '$lib/images/menu/tocino-bbq.png';
 
 export const menu = [
-{
-    nombre: 'hamb',
-    imagen: 'hawaiana',
-    precio: '$35',
-    ingredientes: '50% hamb',
-    detalles: 'juicy'
-}
-]
+	{
+		nombre: 'ARRACHERA',
+		imagen: arrachera,
+		precio: 115,
+		ingredientes: 'Doble carne de arrachera, queso, chorizo, tocino y salchicha para azar'
+	},
+	{
+		nombre: 'CHAMPIÑONES BBQ',
+		imagen: champi,
+		precio: 60,
+		ingredientes: 'Carne de res, queso, champiñones, cebolla caramelizada y BBQ'
+	},
+	{
+		nombre: 'DEDOS DE QUESO',
+		imagen: dedos,
+		precio: 30,
+		ingredientes: '50% dedos, 50% queso'
+	},
+	{
+		nombre: 'PAPAS GAJO',
+		imagen: gajo,
+		precio: 40
+	},
+	{
+		nombre: 'HAWAIANA',
+		imagen: hawai,
+		precio: 60,
+		ingredientes: 'Carne de res, queso, tocino y BBQ'
+	},
+	{
+		nombre: 'SALCHICHA DE RES',
+		imagen: hotRes,
+		precio: 33
+	},
+	{
+		nombre: 'PAPAS FRANCESA',
+		imagen: french,
+		precio: 40
+	},
+	{
+		nombre: 'CHUCKY BURGER',
+		imagen: original,
+		precio: 70,
+		ingredientes: 'Carne de res, chorizo, tocino, salchicha para azar'
+	},
+	{
+		nombre: 'PALOMITAS DE POLLO',
+		imagen: paloma,
+		precio: 30
+	},
+	{
+		nombre: 'TOCINO BBQ',
+		imagen: tocinoBbq,
+		precio: 60,
+		ingredientes: 'Carne de res, queso, tocino y BBQ'
+	}
+];
